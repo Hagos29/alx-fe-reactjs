55bb49e3fbe54f1b086d61b97235efd8b748548c
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-03-06 09:05:42 +0300

    react-router enhanced

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 // src/contexts/AuthContext.jsx
 import React, { createContext, useState, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
